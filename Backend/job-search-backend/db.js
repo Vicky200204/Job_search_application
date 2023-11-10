9dfe83bfea0c3fa2b16d73ecb8b1be46aec72eaa
VIGNESHKUMAR D <125194248+Vicky200204@users.noreply.github.com>
2023-11-10 20:05:37 +0530

    Add files via upload

---
+++ Backend/job-search-backend/db.js
@@ -0,0 +1,17 @@
+// db.js
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost/jobsearch', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
